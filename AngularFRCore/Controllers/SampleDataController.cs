5339cfa6b70c1ad21a2c7fa15d160a8945ac811a
k.volobuev <k.volobuev@fast-report.com>
2021-10-19 07:59:25 +0300

    Infinity loading

--- AngularFRCore/Controllers/SampleDataController.cs
+++ AngularFRCore/Controllers/SampleDataController.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using Microsoft.AspNetCore.Mvc;
 using FastReport.Web;
+using System.IO;
 
 namespace AngularFRCore.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class SampleDataController : ControllerBase
+    [Route("api/[controller]")]
+    public class SampleDataController : Controller
     {
         [HttpGet("[action]")]
         public IActionResult ShowReport(string name)
@@ -14,11 +14,11 @@ namespace AngularFRCore.Controllers
             WebReport WebReport = new WebReport();
             WebReport.Width = "1000";
             WebReport.Height = "1000";
-            WebReport.Report.Load(String.Format("App_Data/{0}.frx", name)); // Load the report into the WebReport object
-            System.Data.DataSet dataSet = new System.Data.DataSet(); // Create a data source
-            dataSet.ReadXml("App_Data/nwind.xml"); // Open the xml database
-            WebReport.Report.RegisterData(dataSet, "NorthWind"); // Registering the data source in the report
-            ViewBag.WebReport = WebReport; // pass the report to View
+            WebReport.Report.Load(String.Format("App_Data/{0}.frx", name)); //Загружаем отчет в объект WebReport
+            System.Data.DataSet dataSet = new System.Data.DataSet(); //Создаем источник данных
+            dataSet.ReadXml("App_Data/nwind.xml"); //Открываем базу данных xml
+            WebReport.Report.RegisterData(dataSet, "NorthWind"); //Регистрируем источник данных в отчете
+            ViewBag.WebReport = WebReport; //передаем отчет во View
             return View();
         }
     }
